97bbb25e96c3dd462a9a81710bc489acc2352b2b
Smriti Goel <smritigoel@Smritis-MacBook-Pro.local>
2017-01-06 10:36:55 -0800

    es6 practice

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,14 @@
-class HoneyMakerBee {
-  // TODO..
-};
+class HoneyMakerBee extends Bee {
+	constructor () {
+		super();
+		this.age = 10;
+		this.honeyPot=0;
+		this.job='make honey';
+	}
+	makeHoney(){
+		this.honeyPot++;
+	}
+	giveHoney(){
+		this.honeyPot--;
+	}
+}
